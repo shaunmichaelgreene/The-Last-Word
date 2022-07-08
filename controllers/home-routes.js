3fe92b8e2a3c07689357af1be6bcebcb73a452ba
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-07 20:36:43 -0400

    fixed folder structure for views, corrected typo in home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,6 @@ const { Post, User, Comment } = require('../models');
 
 //load all posts for the homepage
 router.get('/', (req, res) => {
-    console.log('=====================');
     Post.findAll({
         attributes: [
             'id',
@@ -14,7 +13,7 @@ router.get('/', (req, res) => {
         include: [
             {
                 model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user-id', 'created_at'],
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
                 include: {
                     model: User,
                     attributes: ['username']
@@ -29,7 +28,7 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
 
-        res.render('homepage', {
+        res.render('homepage.handlebars', {
             posts,
             loggedIn: req.session.loggedIn
         });
