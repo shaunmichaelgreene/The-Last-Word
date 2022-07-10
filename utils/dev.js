1bbecf86a962a93b329657e4bb2880083b026e50
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-10 19:43:41 -0400

    testig virtual field for Post model

--- utils/dev.js
+++ utils/dev.js
@@ -26,5 +26,29 @@ if (comments) {
 
 // formatDistanceToNow(lastCommentTime, )
 
+//test differenceInHours method, where testDate2 = current date & testDate3 = 7/8/2022 @ 10:30PM) - prints 45
 var hoursDifference = differenceInHours(testDate2, testDate3)
 console.log(hoursDifference)
+
+//logic for the isActive property
+
+function isActive(dbPostData) {
+    //deconstruct dbPostData to establish easier to use variables for the time of the Post, last comment of the post, and time of the last comment. Also convert the difference in the postTime and currentTime for the post into a usable value (and do the same for the most recent comment), using the date-fns node package's differentInHours method
+    currentTime = new Date()
+    postTime = dbPostData.created_at
+    var postHoursDifference = differenceInHours(currentTime, postTime)
+    if (dbPostData.comments) {
+        comments = dbPostData.comments
+        lastComment = comments[comments.length - 1]
+        lastCommentTime = lastComment.created_at;
+        lastCommentHoursDifference = differenceInHours(currentTime, lastCommentTime)
+    }
+
+    if (postHoursDifference < 24) {
+        return true //(post IS active)
+    } else if (comments.length == 0) {
+        return false //(post IS NOT active)
+    } else if (lastCommentHoursDifference < 24) {
+        return true
+    } else return false
+}
