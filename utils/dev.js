3f6860136459588730b5ebf50da24a0a1af60a8b
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-10 20:35:21 -0400

    working on conditional logic for locking a post

--- utils/dev.js
+++ utils/dev.js
@@ -33,7 +33,8 @@ console.log(hoursDifference)
 //logic for the isActive property
 
 function isActive(dbPostData) {
-    //deconstruct dbPostData to establish easier to use variables for the time of the Post, last comment of the post, and time of the last comment. Also convert the difference in the postTime and currentTime for the post into a usable value (and do the same for the most recent comment), using the date-fns node package's differentInHours method
+    //deconstruct dbPostData to establish easier to use variables for the time of the Post, last comment of the post, and time of the last comment. Also convert the difference in the postTime and currentTime for the post into a usable value (and do the same for the most recent comment), using the date-fns node package's differenceInHours method
+    timeLimit = 24
     currentTime = new Date()
     postTime = dbPostData.created_at
     var postHoursDifference = differenceInHours(currentTime, postTime)
@@ -42,13 +43,15 @@ function isActive(dbPostData) {
         lastComment = comments[comments.length - 1]
         lastCommentTime = lastComment.created_at;
         lastCommentHoursDifference = differenceInHours(currentTime, lastCommentTime)
+    } else {
+        var noComments = true //if dbPost.Data.comments doesn't exist, then there are no comments
     }
 
-    if (postHoursDifference < 24) {
-        return true //(post IS active)
-    } else if (comments.length == 0) {
-        return false //(post IS NOT active)
-    } else if (lastCommentHoursDifference < 24) {
-        return true
-    } else return false
+    if (postHoursDifference < timeLimit) {
+        return true //(post is less than 24 hours old, post IS active no matter what)
+    } else if (noComments) {
+        return false //(post is older than 24 hrs, no comments exist, post IS NOT active)
+    } else if (lastCommentHoursDifference < timeLimit) {
+        return true //(post is older than 24 hours, comments exist but since most recent is younger than 24 hrs,  post is still active)
+    } else return false //(comments exist, but since most recent is older than 24 hours, post is NOT active)
 }
